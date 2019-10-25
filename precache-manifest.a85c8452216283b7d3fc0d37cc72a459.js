495d35202d09e9a91071b20fa0c6b0d7dab03f9e
NewChargeXO@gmail.com <birader.priyanka@gmail.com>
2019-10-25 20:11:59 +0530

    Updates

---
+++ precache-manifest.a85c8452216283b7d3fc0d37cc72a459.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4317d6179d8abe366a125921492cbcb",
+    "url": "/NewRevolutionXO/index.html"
+  },
+  {
+    "revision": "357137999b68326b14bf",
+    "url": "/NewRevolutionXO/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c86fadda260fa47a5817",
+    "url": "/NewRevolutionXO/static/css/main.64f4599e.chunk.css"
+  },
+  {
+    "revision": "357137999b68326b14bf",
+    "url": "/NewRevolutionXO/static/js/2.1d184221.chunk.js"
+  },
+  {
+    "revision": "c86fadda260fa47a5817",
+    "url": "/NewRevolutionXO/static/js/main.04e6e735.chunk.js"
+  },
+  {
+    "revision": "5fa863a59d145423e325",
+    "url": "/NewRevolutionXO/static/js/runtime-main.10cc9c2f.js"
+  },
+  {
+    "revision": "e2dc9132df9ed74d23bc1387b3827a3b",
+    "url": "/NewRevolutionXO/static/media/Aging.e2dc9132.svg"
+  },
+  {
+    "revision": "25d87b14dbf199acd9265b0d1d9e4eda",
+    "url": "/NewRevolutionXO/static/media/Applications.25d87b14.svg"
+  },
+  {
+    "revision": "42379e99caea3b369064c4bc1151d6fc",
+    "url": "/NewRevolutionXO/static/media/BusinessTech.42379e99.svg"
+  },
+  {
+    "revision": "f5c1b54d443e44addd4f97d214c4b539",
+    "url": "/NewRevolutionXO/static/media/CalenderingAnode.f5c1b54d.svg"
+  },
+  {
+    "revision": "0ba5217fb4f219bd5372ae75bb8992c7",
+    "url": "/NewRevolutionXO/static/media/CalenderingCathode.0ba5217f.svg"
+  },
+  {
+    "revision": "5e3235d7c14fd2ea063869c4ca088847",
+    "url": "/NewRevolutionXO/static/media/Cell.5e3235d7.svg"
+  },
+  {
+    "revision": "f127f28e6c23384f5ff8041aa1f2b2bb",
+    "url": "/NewRevolutionXO/static/media/CellManufacturing.f127f28e.png"
+  },
+  {
+    "revision": "5ae97d5077f0f6955b97c1b5e7ca1b3a",
+    "url": "/NewRevolutionXO/static/media/CellTech.5ae97d50.svg"
+  },
+  {
+    "revision": "c08b16b6327cb563e146466719ddffe4",
+    "url": "/NewRevolutionXO/static/media/CellTechnology.c08b16b6.gif"
+  },
+  {
+    "revision": "1918bb361401297599c8872215ffdf15",
+    "url": "/NewRevolutionXO/static/media/ChargeDis.1918bb36.svg"
+  },
+  {
+    "revision": "8b54267c2dd6ec3af90ef1090828f276",
+    "url": "/NewRevolutionXO/static/media/ChargeDischarge.8b54267c.svg"
+  },
+  {
+    "revision": "3073e36d895b3aaaa52d5eb213583c1d",
+    "url": "/NewRevolutionXO/static/media/CoatingAnode.3073e36d.svg"
+  },
+  {
+    "revision": "45495e8bca2e7d91abafdb0ebfb583ac",
+    "url": "/NewRevolutionXO/static/media/CoatingCathode.45495e8b.svg"
+  },
+  {
+    "revision": "2fbd8691c063d2fff0057c3282fe96e8",
+    "url": "/NewRevolutionXO/static/media/CoreTech.2fbd8691.svg"
+  },
+  {
+    "revision": "df9f723b9230c0a4e0dbd1648d7bad69",
+    "url": "/NewRevolutionXO/static/media/Degassing.df9f723b.svg"
+  },
+  {
+    "revision": "524be1d9fc7992aba1373c6a9e5cfc72",
+    "url": "/NewRevolutionXO/static/media/ElectrolyteFilling.524be1d9.svg"
+  },
+  {
+    "revision": "2f745732d1f2428cddd099302aa41dde",
+    "url": "/NewRevolutionXO/static/media/HeaderLogo.2f745732.svg"
+  },
+  {
+    "revision": "1a071d7ae0c8ae87bcd3080203a3ab18",
+    "url": "/NewRevolutionXO/static/media/Home.1a071d7a.svg"
+  },
+  {
+    "revision": "791bc45d11c2a164049bd6b06c71e660",
+    "url": "/NewRevolutionXO/static/media/LFP.791bc45d.svg"
+  },
+  {
+    "revision": "f139cdbb5e073c03fb06a008f5744f54",
+    "url": "/NewRevolutionXO/static/media/LFPNMC.f139cdbb.svg"
+  },
+  {
+    "revision": "bed950520a811b278b3e1afd32251b22",
+    "url": "/NewRevolutionXO/static/media/LithiumIon.bed95052.GIF"
+  },
+  {
+    "revision": "e187017f0beb25e659eb6ab15c29576c",
+    "url": "/NewRevolutionXO/static/media/LithiumIon.e187017f.svg"
+  },
+  {
+    "revision": "75f5b6065e35a1d08970eef1fed9d298",
+    "url": "/NewRevolutionXO/static/media/MgmtExp.75f5b606.svg"
+  },
+  {
+    "revision": "8cbc5ebf75184de2ed0e78e179238668",
+    "url": "/NewRevolutionXO/static/media/MixerAnode.8cbc5ebf.svg"
+  },
+  {
+    "revision": "6e67ef12d8a59416334302baf208ec16",
+    "url": "/NewRevolutionXO/static/media/MixerCathode.6e67ef12.svg"
+  },
+  {
+    "revision": "45340fc412257d3c96009e2fbc50fcfd",
+    "url": "/NewRevolutionXO/static/media/Module.45340fc4.svg"
+  },
+  {
+    "revision": "3e4a4d680f7a2c0b4de2174550f8df68",
+    "url": "/NewRevolutionXO/static/media/NMC.3e4a4d68.svg"
+  },
+  {
+    "revision": "3d077402dbbe02325edf0703575d6ad2",
+    "url": "/NewRevolutionXO/static/media/RollFormation.3d077402.svg"
+  },
+  {
+    "revision": "35ef1b127d6f2b4368104e8aecd1bc06",
+    "url": "/NewRevolutionXO/static/media/SlitteringAnode.35ef1b12.svg"
+  },
+  {
+    "revision": "baaf5c7368aa5b141cf67fb4f668cb8c",
+    "url": "/NewRevolutionXO/static/media/SlitteringCathode.baaf5c73.svg"
+  },
+  {
+    "revision": "49cf1d4fa5f908472baa24c8fab6c729",
+    "url": "/NewRevolutionXO/static/media/SrikanthMgb.49cf1d4f.jpg"
+  },
+  {
+    "revision": "7cc2372f9b8055c726ea16d13d8d2c63",
+    "url": "/NewRevolutionXO/static/media/TechCollaboration.7cc2372f.svg"
+  },
+  {
+    "revision": "1a48bcae52fa7c02d860c85e46119194",
+    "url": "/NewRevolutionXO/static/media/frontImg.1a48bcae.svg"
+  },
+  {
+    "revision": "133f1b9f1333042d17f6ff371a9bdded",
+    "url": "/NewRevolutionXO/static/media/left.133f1b9f.svg"
+  },
+  {
+    "revision": "042364db12a73da2bc4a404e07ec3fc2",
+    "url": "/NewRevolutionXO/static/media/logo.042364db.svg"
+  },
+  {
+    "revision": "a8891122d33dceaa6a5c1d700e3dce90",
+    "url": "/NewRevolutionXO/static/media/right.a8891122.svg"
+  }
+]);
